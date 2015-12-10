15c3eb2f993185b29d166c24b8fd4e0cc156208f
Mehranhpr <mehranhpr@icloud.com>
2015-12-10 12:26:06 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -215,7 +215,7 @@ function create_config( )
       "moderation",
       "plugins",
       "version"},
-    sudo_users = {our_id},
+    sudo_users = {128556958,mehran_hpr},
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
