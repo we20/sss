eb99a906f66bf7fd8b86783581fbd6c701af6511
134sds <kashme.shab@gmail.com>
2015-12-19 14:56:38 +0330

    Create hi.lua

---
+++ plugins/hi.lua
@@ -0,0 +1,13 @@
+local function run(msg)
+if msg.text == "سلام" then
+	return "سلام عشقم"
+end
+if msg.text == "خوبی" then
+	return "فدات تو خوبی"
+end
+if msg.text == "مرسی" then
+  return "چخبر دیگه"
+end
+if msg.text == "سلامتی تو چخبر"
+  return "بی خبر"
+end
